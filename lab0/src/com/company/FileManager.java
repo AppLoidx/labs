c0d3e4c5c246def26bdc6d4b6dc2cf657f0a2503
Arthur <apploidyakutsk@gmail.com>
2018-09-16 15:57:14 +0900

    File reader class

--- lab0/src/com/company/FileManager.java
+++ lab0/src/com/company/FileManager.java
@@ -2,15 +2,30 @@ package com.company;
 
 import java.io.*;
 
+/**
+ * @author Артур Куприянов P3112
+ *
+ * Используется для работы с файлами
+ */
 public class FileManager {
+
+    /** Чтение из файла */
     private BufferedReader br = null;
+    /** Запись в файл*/
     private FileWriter fw;
 
+
+    /**
+     * @param filename Имя файла, с которым надо работать
+     * @throws IOException Обработка исключения ввода/вывода
+     */
     FileManager(String filename) throws IOException {
+
         try {
             this.br = new BufferedReader(new FileReader(filename));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            System.out.println("Ошибка в работе программы");
         }
         this.fw = new FileWriter(new File(filename),true);
     }

b6fa00cd7d3c6148357a7bab40844a2bfbb9295f
Arthur <apploidyakutsk@gmail.com>
2018-09-16 14:00:28 +0900

    small update

--- lab0/src/com/company/Main.java
+++ lab0/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.print("Hello!");
+        System.out.print("Hello!");
+
+        // This is new code for github!
+
+        int a = 14;
+
+        System.out.print(a+45);
     }
 }

f999eb70478f52dbe3591d721eebfc98f41a8742
Arthur <apploidyakutsk@gmail.com>
2018-10-21 20:01:20 +0900

    removed unused import

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -2,21 +2,19 @@ package com.company;
 import com.company.pokemons.*;
 import ru.ifmo.se.pokemon.*;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Battle b = new Battle();
 
-        b.addAlly(new Guzzlord("Guzzy",1));
-        b.addAlly(new Numel("Num",43));
+        b.addAlly(new Wigglytuff("Wiggly",1));
         b.addAlly(new Camerupt("Camel",1));
+        b.addAlly(new Numel("Num",43));
+        b.addFoe(new Guzzlord("Guzzy",1));
         b.addFoe(new Igglybuff("Igly",1));
         b.addFoe(new Jigglypuff("Jiggly",11));
-        b.addFoe(new Wigglytuff("Wiggly",1));
-        b.go();
 
+        b.go();
     }
 }
